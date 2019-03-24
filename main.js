7c67fcabd38be4d088d2b5c478321c3affb6cb9a
HBandra <hbandrovska@gmail.com>
2019-03-24 21:16:00 +0200

    commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+function audioA(){
+    document.getElementById('audio_a').play();
+}
+function audioS(){
+    document.getElementById('audio_s').play();
+}
+function audioD(){
+    document.getElementById('audio_d').play();
+}
+function audioF(){
+    document.getElementById('audio_f').play();
+}
+function audioG(){
+    document.getElementById('audio_g').play();
+}
+function audioH(){
+    document.getElementById('audio_h').play();
+}
+function audioJ(){
+    document.getElementById('audio_j').play();
+}
+function audioK(){
+    document.getElementById('audio_k').play();
+}
+let buttonA = document.getElementById('button_A').addEventListener('click', audioA);
+let buttonS = document.getElementById('button_S').addEventListener('click', audioS);
+let buttonD = document.getElementById('button_D').addEventListener('click', audioD);
+let buttonF = document.getElementById('button_F').addEventListener('click', audioF);
+let buttonG = document.getElementById('button_G').addEventListener('click', audioG);
+let buttonH = document.getElementById('button_H').addEventListener('click', audioH);
+let buttonJ = document.getElementById('button_J').addEventListener('click', audioJ);
+let buttonK = document.getElementById('button_K').addEventListener('click', audioK);
